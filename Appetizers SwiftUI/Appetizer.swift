bcea74c99dbf14251b2dd4979a2d4a541bb14cb7
Catalin Palade <catalin@recruitd.com>
2020-12-29 12:26:29 +0000

    datamodel for appetizer + mockdata for sample, AppetizerListCell - UI

---
+++ Appetizers SwiftUI/Appetizer.swift
@@ -0,0 +1,38 @@
+//
+//  Appetizer.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 29/12/2020.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    
+    let id:             Int
+    let name:           String
+    let description:    String
+    let price:          Double
+    let imageURL:       String
+    let calories:       Int
+    let protein:        Int
+    let carbs:          Int
+    
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 23,
+                                           carbs: 5)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
---
+++ Appetizers SwiftUI/AppetizerListCell.swift
@@ -0,0 +1,39 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 29/12/2020.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    
+    var appetizer: Appetizer
+    
+    var body: some View {
+        HStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct AppetizerViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListCell(appetizer: MockData.sampleAppetizer)
+    }
+}
