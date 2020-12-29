9c4648e67c8193bd714cad5e5fdd5e9e56b48ee9
Catalin Palade <catalin@recruitd.com>
2020-12-29 15:10:29 +0000

    NetworkManager - singleton, custom errors

---
+++ Appetizers SwiftUI/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 29/12/2020.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
