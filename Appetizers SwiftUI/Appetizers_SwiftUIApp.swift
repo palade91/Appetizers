66e8bf85e70abee4c805af961d764edbb5f23517
Catalin Palade <catalin@recruitd.com>
2020-12-29 10:02:11 +0000

    Initial Commit

---
+++ Appetizers SwiftUI/Appetizers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_SwiftUIApp.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
