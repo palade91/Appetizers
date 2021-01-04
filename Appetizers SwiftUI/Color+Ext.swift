7e8916ddd8dab3ddacdb2ae1bdf3308a597f5061
Catalin Palade <catalin@recruitd.com>
2021-01-04 17:44:40 +0000

    ActivityIndicator - UIViewRepresentable + Color extension

---
+++ Appetizers SwiftUI/Color+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Ext.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 04/01/2021.
+//
+
+import SwiftUI
+import UIKit
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
---
+++ Appetizers SwiftUI/LoadingView.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingView.swift
+//  Appetizers SwiftUI
+//
+//  Created by Catalin Palade on 04/01/2021.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+        activityIndicatorView.color = UIColor.brandPrimary
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            ActivityIndicator()
+        }
+    }
+}
